70f4b26b43ed227ea2d99db99b1fe8d8d7e5eb67
Ryan <ryaangu@gmail.com>
2022-02-18 05:55:28 -0300

    Updated build.sh to show exit code.

--- tests/build.sh
+++ tests/build.sh
@@ -5,3 +5,5 @@ dotnet run ../tests/test.su ../tests/libc.su ../tests/common.su
 # Compile the output files
 cd ..
 g++ -I tests/out/ tests/out/files/**/*.cpp -fno-exceptions && ./a.out
+
+echo Exit code: $?
